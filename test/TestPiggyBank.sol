6fda2b58edbfc8c863e30c6cfc78a5dec456ba30
sai Kumar <syerubandi@gmail.com>
2017-06-25 08:44:26 +1000

    test added

--- test/TestPiggyBank.sol
+++ test/TestPiggyBank.sol
@@ -11,7 +11,18 @@ contract TestPiggyBank {
 
     uint expected = 0;
 
-    Assert.equal(piggyBank.checkBalance(), expected, "A new piggy account should start with zero balance");
+    Assert.equal(piggyBank.checkBalance(), expected, "A new Piggy account should start with zero balance");
+  }
+
+  function testBalanceAfterDeposit() {
+    PiggyBank piggyBank = new PiggyBank();
+
+    piggyBank.deposit(123);
+
+    uint expected = 123;
+
+    Assert.equal(piggyBank.checkBalance(), expected, "A Piggy account should show the correct balance");
+
   }
 
   function testDepositCanbeWithdrawn() {
@@ -19,8 +30,24 @@ contract TestPiggyBank {
 
     piggyBank.deposit(123);
 
-    Assert.equal(piggyBank.withdraw(123), true, "A new piggy account should start with zero balance");
+    Assert.equal(piggyBank.withdraw(123), true, "Should be able to withdraw the deposit ");
 
   }
+
+  function testBalanceCanbeWithdrawn() {
+    PiggyBank piggyBank = new PiggyBank();
+    uint deposit1 = 123;
+    uint deposit2 = 456;
+
+    piggyBank.deposit(deposit1);
+    piggyBank.deposit(deposit2);
+
+    uint expected = deposit1 + deposit2;
+
+    Assert.equal(piggyBank.checkBalance(), expected, "Piggy balance should reflect the sum of deposits if no withdraws");
+    Assert.equal(piggyBank.withdraw(piggyBank.checkBalance()), true, "Should be able to withdraw balance");
+    Assert.equal(piggyBank.checkBalance(),0,"Account balance should be zero after balance withdrawn");
+  }
+
 
 }
