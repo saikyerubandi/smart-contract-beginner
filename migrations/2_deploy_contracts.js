42e1e1b19bfd72219e31ecdab4287d0c9e6b8dc3
sai Kumar <syerubandi@gmail.com>
2017-06-25 07:36:28 +1000

    update migration for PiggyBank

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Counter = artifacts.require("./Counter.sol");
+var PiggyBank = artifacts.require("./PiggyBank.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Counter);
+  deployer.deploy(PiggyBank);
 };
