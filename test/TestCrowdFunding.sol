2e2e4e1f0d279694ed3671ae9a4cc3b322c2bbf1
sai Kumar <syerubandi@gmail.com>
2017-07-08 11:16:18 +1000

    new test for CrowdFund

--- test/TestCrowdFunding.sol
+++ test/TestCrowdFunding.sol
@@ -11,6 +11,7 @@ contract TestCrowdFunding {
     CrowdFunding crowdFunding = CrowdFunding(DeployedAddresses.CrowdFunding());
     crowdFunding.newCampaign(msg.sender,100);
 
+
     uint expected = 100;
     var (beneficiary,goalAmount,numFunders,amountRaised)  = crowdFunding.getCampaign(0);
     //Assert.typeOf(beneficiary,address);
@@ -19,14 +20,14 @@ contract TestCrowdFunding {
 
  function testFundingACampaign() {
    CrowdFunding crowdFunding = CrowdFunding(DeployedAddresses.CrowdFunding());
-   crowdFunding.newCampaign(msg.sender,100);
+   crowdFunding.newCampaign(msg.sender,100);//beneficiary and goalamount set
    crowdFunding.fundCampaign(0);
    //log1('msg.sender',msg.sender);
    //log1('msg.value',msg.value);
    uint expected = 100;
    var (beneficiary,goalAmount,numFunders,amountRaised)  = crowdFunding.getCampaign(0);
    //Assert.typeOf(beneficiary,address);
-   Assert.equal(amountRaised,msg.value,"A new campaign should have a fundingoal");
+   Assert.equal(goalAmount,expected,"A new campaign should have a funding goal");
 
  }
 
